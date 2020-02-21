b478659b058f740955f20a9070c438efca5e3c04
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-02-21 18:52:07 +0000

    scroll animations added

--- scripts/app.js
+++ scripts/app.js
@@ -39,7 +39,6 @@ function runPorftolio() {
 
 
   const skillIcons = document.getElementsByClassName('skillIcon')
-  console.log(skillIcons[0])
 
   Array.from(skillIcons).forEach(function (icon) {
     icon.addEventListener('mouseover', () => {
@@ -99,7 +98,89 @@ function runPorftolio() {
   //   })
   // })
 
+  // ---------------------Scroll Events ------------------------------
+
+  const skill = document.querySelectorAll('.skillIcon')
+  // const interests = document.getElementsByClassName(interests)
+
+  var scroll = window.requestAnimationFrame ||
+    function (callback) { window.setTimeout(callback, 1000 / 60) }
+
+  var elementsToShow = document.querySelectorAll('.show-on-scroll')
+  // var elementsToShow2 = document.querySelectorAll('.show-on-scroll-right')
+
+  let time = 300
+  let complete = true
+
+  function loop1() {
+    elementsToShow.forEach(function (element) {
+      if (isElementInViewport(element)) {
+        if (element.className.includes('leftScroll')) {
+          setTimeout(() => {
+            element.classList.add('is-visible-left')
+          }, 0)
+        } else if (element.className.includes('rightScrollSlow')) {
+          setTimeout(() => {
+            element.classList.add('is-visible-right')
+          }, 800)
+        } else if (element.className.includes('rightScroll')) {
+          element.classList.add('is-visible-right')
+        } else if (element.className.includes('fadeIn')) {
+          element.classList.add('fade-in')
+        }
+        if (element.className.includes('skills')) {
+          if (complete) {
+            for (let i = 0; i < element.children.length; i++) {
+              setTimeout(() => {
+                element.children[i].classList.add('drop-in')
+              }, time)
+              time += 400
+            }
+            complete = false
+          }
+        }
+      } else {
+        // element.classList.remove('is-visible-left')
+        // element.classList.remove('is-visible-right')
+        element.classList.remove('fade-in')
+      }
+    })
+    scroll(loop1)
+  }
+
+  loop1()
+
+  // function loop2() {
+  //   elementsToShow2.forEach(function (element) {
+  //     if (isElementInViewport(element)) {
+  //       setTimeout(() => {
+  //         element.classList.add('is-visible-right')
+  //       }, 500)
+  //     } else {
+  //       element.classList.remove('is-visible-right')
+  //     }
+  //   })
+  //   scroll(loop2)
+  // }
+
+  // loop2()
+
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect()
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    )
+  }
 
 }
+
+
 
 document.addEventListener('DOMContentLoaded', runPorftolio)
